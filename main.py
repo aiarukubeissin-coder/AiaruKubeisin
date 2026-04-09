b05ba8ea7877551dc53ca886b35ca1b04b9d5330
aiarukubeissin-coder <aiaru.kubeissin@narxoz.kz>
2026-04-09 15:17:08 +0500

    added in 13 week

--- main.py
+++ main.py
@@ -6,8 +6,8 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'dfgdhgvjh, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+    print(f'dfasaSsgdhgvjh, {name}')  # Press ⌘F8 to toggle the breakpoint.
+as
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
